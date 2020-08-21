8b125107f2fc241481fcc6bae28730bf48dece85
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-20 20:43:01 -0400

    adding user class

---
+++ user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self,name,email)
+        self.name = name 
+        self.email =email
+    def __str__(self):
+        return (f"Username: \"{self.name}\" | email: {self.email}")
