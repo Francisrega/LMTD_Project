3d26fdc790240b65087f0be1026c5031b5e955ed
dolmagrg1231 <dolmagurung75@gmail.com>
2020-08-28 22:38:04 -0400

    Deleted Files that are not needed

--- user.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self,name,email):
-        self.name = name 
-        self.email =email
-    def __str__(self):
-        return (f"Username: \"{self.name}\" | email: {self.email}")
